fa96c725ac1090af424712430c4556173866cbb9
Geriel-L <gerielliberato73@gmail.com>
2024-08-12 13:27:35 +0000

    i did a lot

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// console.log('hello world')
+console.log('hello world')
 
 // var prompt = prompt('Whats your name?');
 
@@ -11,12 +11,87 @@
 // }
 
 
-var age = prompt('How old are you?');
+//  var age = prompt('How old are you?');
 
-if (age < 18 && typeof age === "string") {
-  alert('You are a minor!')
-} else if (age >= 18 && age <= 60) {
-  alert('You are an adult!')
-} else {
-  alert('DAMN YOUR OLD ASF')
+//  if (age < 18 && typeof age === "string") {
+//   alert('You are a minor!')
+// } else if (age >= 18 && age <= 60) {
+//   alert('You are an adult!')
+// } else {
+//   alert('DAMN YOUR OLD ASF')
+// }
+
+function areYouOldEnough (age) {
+  var age = prompt('How old are you?');
+
+  if (age < 18 && typeof age === "string") {
+    alert('You are a minor!')
+  } else if (age >= 18 && age <= 60) {
+    alert('You are an adult!')
+  } else {
+    alert('DAMN YOUR OLD ASF')
+  }
 }
+
+
+
+function introduction(name, age,location) {
+ console.log(name)
+ console.log(age)
+ console.log(location)
+console.log('My name is ' + name + 'my age is ' + age + ' I live in ' + location)
+}
+
+introduction('bob ',21 ,'NYC')
+
+ function Uploadandidentifyplantid() {
+  // get the photo from the input button
+  var photoInput = document.getElementById('photoInput');
+  console.log(photoInput); 
+  console.log(photoInput.files[0]);
+
+  // if no photo is selected was slelected let the user know
+  if (photoInput.files.length === 0) {
+    alert('upload a photo');
+  return
+
+  }
+
+var selectedFile = photoInput.files[0];
+
+  var Reader = new FileReader();
+
+// Set up event handeler when the submit button is clicked to read the file and set up the API call
+  Reader.onload = function (e){
+    // Store image in a var
+   var base64image = e.target.result;
+  console.log(base64image);
+
+    // Create variables for api call
+    var apikey = 'MsyfBz4bd54Pa6GTQCvcnwrtU1TG7ULrSDFAeJZSiCklzEBpaA';
+    var latitude = 49.207;
+    var longitude = 16.608
+    var health = 'all';
+    var SimliarImages = true;
+    var details = 'common_names,url,description,taxonomy,rank,gbif_id,inaturalist_id,image,synonyms,edible_parts,watering,propagation_methods,treatment,cause';
+    var language = 'en';
+    var apiPLANTIDurl = `https://plant.id/api/v3/identification?details=${details}&language=${language}`;
+  }
+// Make our first api call to the plant id data base
+axios.post(
+  apiPLANTIDurl,
+  {
+    images: [base64image],
+   latitude: latitude,
+   longitude: longitude,
+   health: health,
+  SimliarImages: SimliarImages
+  },
+    {
+      headers: {
+       'Api-key': apikey, 
+        'Content-Type' : 'application/json'
+      }
+    }  
+  )
+ } 
